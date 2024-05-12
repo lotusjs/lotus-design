dbc21fbdadee253ee906dffad58bc73f319f075d
wangxingkang <wang_xingkang@qq.com>
2024-05-12 18:11:56 +0800

    chore: project init

---
+++ test/setup.ts
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom/vitest';
+
+global.ResizeObserver = class ResizeObserver {
+  cb: any;
+  constructor(cb: any) {
+    this.cb = cb;
+  }
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }]);
+  }
+  unobserve() {}
+  disconnect() {}
+};
