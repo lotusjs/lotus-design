5ac386f53270c45c555b6dbb58ea510311029fec
wangxingkang <wang_xingkang@qq.com>
2024-05-24 11:52:07 +0800

    feat: add @lotus-design/react-slot

---
+++ primitives/slot/src/SlotClone.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef, isValidElement, cloneElement, Children } from 'react';
+import { composeRefs } from '@lotus-design/react-compose-refs'
+import { mergeProps } from './utils'
+
+interface SlotCloneProps {
+  children: React.ReactNode;
+}
+
+export const SlotClone = forwardRef<any, SlotCloneProps>(
+  (props, forwardedRef) => {
+    const { children, ...slotProps } = props;
+
+    if (isValidElement(children)) {
+      return cloneElement(children, {
+        ...mergeProps(slotProps, children.props),
+        // @ts-expect-error
+        ref: forwardedRef ? composeRefs(forwardedRef, (children as any).ref) : (children as any).ref,
+      })
+    }
+
+    return Children.count(children) > 1 ? Children.only(null) : null;
+  }
+)
---
+++ primitives/slot/src/index.ts
@@ -0,0 +1,3 @@
+export { Slottable } from './Slottable'
+export { SlotClone } from './SlotClone'
+export { Slot, type SlotProps } from './Slot'
