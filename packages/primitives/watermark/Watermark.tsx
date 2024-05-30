91aee9b82da9259cd7d497d5365f7c447d0f1eec
wangxingkang <wang_xingkang@qq.com>
2024-05-30 16:43:04 +0800

    feat: add Watermark

---
+++ packages/primitives/watermark/Watermark.tsx
@@ -0,0 +1,195 @@
+import React, { forwardRef, useMemo, useState, useEffect } from 'react'
+import { Primitive } from '../primitive';
+import { getPixelRatio, reRendering } from './utils';
+import { useClips, FontGap } from './hooks/useClips'
+import { useWatermark } from './hooks/useWatermark'
+
+import type { WatermarkProps, WatermarkElement } from './interface'
+
+const DEFAULT_GAP_X = 100;
+const DEFAULT_GAP_Y = 100;
+
+export const Watermark = forwardRef<WatermarkElement, WatermarkProps>(
+  (props, forwardedRef) => {
+    const {
+      className,
+      style,
+      font = {},
+      gap = [DEFAULT_GAP_X, DEFAULT_GAP_Y],
+      offset,
+      width,
+      height,
+      image,
+      content,
+      zIndex = 9,
+      rotate = -22,
+      children,
+    } = props;
+    const {
+      color = 'rgba(0,0,0,.15)',
+      fontSize = 16,
+      fontWeight = 'normal',
+      fontStyle = 'normal',
+      fontFamily = 'sans-serif',
+      textAlign = 'center',
+    } = font;
+    const [gapX = DEFAULT_GAP_X, gapY = DEFAULT_GAP_Y] = gap;
+    const gapXCenter = gapX / 2;
+    const gapYCenter = gapY / 2;
+    const offsetLeft = offset?.[0] ?? gapXCenter;
+    const offsetTop = offset?.[1] ?? gapYCenter;
+
+    const markStyle = useMemo(
+      () => {
+        const mergedStyle: React.CSSProperties = {
+          zIndex,
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          height: '100%',
+          pointerEvents: 'none',
+          backgroundRepeat: 'repeat',
+        };
+
+        /** Calculate the style of the offset */
+        let positionLeft = offsetLeft - gapXCenter;
+        let positionTop = offsetTop - gapYCenter;
+        if (positionLeft > 0) {
+          mergedStyle.left = `${positionLeft}px`;
+          mergedStyle.width = `calc(100% - ${positionLeft}px)`;
+          positionLeft = 0;
+        }
+        if (positionTop > 0) {
+          mergedStyle.top = `${positionTop}px`;
+          mergedStyle.height = `calc(100% - ${positionTop}px)`;
+          positionTop = 0;
+        }
+        mergedStyle.backgroundPosition = `${positionLeft}px ${positionTop}px`;
+
+        return mergedStyle;
+      },
+      [zIndex, offsetLeft, gapXCenter, offsetTop, gapYCenter]
+    )
+
+    const [container, setContainer] = useState<HTMLDivElement | null>();
+    const [subElements, setSubElements] = useState(new Set<HTMLElement>());
+
+    const targetElements = useMemo(() => {
+      const list = container ? [container] : [];
+      return [...list, ...Array.from(subElements)];
+    }, [container, subElements]);
+
+    // ============================ Content =============================
+
+    const getMarkSize = (ctx: CanvasRenderingContext2D) => {
+      let defaultWidth = 120;
+      let defaultHeight = 64;
+
+      if (!image) {
+        ctx.font = `${Number(fontSize)}px ${fontFamily}`;
+        const contents = Array.isArray(content) ? content : [content];
+        const sizes = contents.map((item) => {
+          const metrics = ctx.measureText(item!);
+
+          return [metrics.width, metrics.fontBoundingBoxAscent + metrics.fontBoundingBoxDescent];
+        });
+
+        defaultWidth = Math.ceil(Math.max(...sizes.map((size) => size[0])));
+        defaultHeight =
+          Math.ceil(Math.max(...sizes.map((size) => size[1]))) * contents.length +
+          (contents.length - 1) * FontGap;
+      }
+
+      return [width ?? defaultWidth, height ?? defaultHeight] as const;
+    }
+
+    const getClips = useClips();
+
+    const [watermarkInfo, setWatermarkInfo] = useState<[base64: string, contentWidth: number]>(
+      null!,
+    );
+
+    const renderWatermark = () => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+
+      if (ctx) {
+        const ratio = getPixelRatio();
+        const [markWidth, markHeight] = getMarkSize(ctx);
+
+        const drawCanvas = (
+          drawContent?: NonNullable<WatermarkProps['content']> | HTMLImageElement,
+        ) => {
+          const [nextClips, clipWidth] = getClips(
+            drawContent || '',
+            rotate,
+            ratio,
+            markWidth,
+            markHeight,
+            {
+              color,
+              fontSize,
+              fontStyle,
+              fontWeight,
+              fontFamily,
+              textAlign,
+            },
+            gapX,
+            gapY,
+          );
+
+          setWatermarkInfo([nextClips, clipWidth]);
+        }
+
+        if (image) {
+          const img = new Image();
+          img.onload = () => {
+            drawCanvas(img);
+          };
+          img.onerror = () => {
+            drawCanvas(content);
+          };
+          img.crossOrigin = 'anonymous';
+          img.referrerPolicy = 'no-referrer';
+          img.src = image;
+        } else {
+          drawCanvas(content);
+        }
+      }
+    }
+
+    // ============================= Effect =============================
+
+    const [appendWatermark, removeWatermark, isWatermarkEle] = useWatermark(markStyle);
+    useEffect(() => {
+      if (watermarkInfo) {
+        targetElements.forEach((holder) => {
+          appendWatermark(watermarkInfo[0], watermarkInfo[1], holder);
+        });
+      }
+    }, [watermarkInfo, targetElements]);
+
+    // ============================ Observe =============================
+
+    const onMutate = (mutations: MutationRecord[]) => {
+      mutations.forEach((mutation) => {
+        if (reRendering(mutation, isWatermarkEle)) {
+          // syncWatermark();
+        }
+      });
+    };
+
+    // ============================= Render =============================
+
+    return (
+      <Primitive.div
+        ref={setContainer}
+        className={className}
+        style={{ position: 'relative', ...style }}
+      >
+        {children}
+      </Primitive.div>
+    )
+  }
+)
---
+++ packages/react/src/watermark/interface.ts
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export interface WatermarkProps {
+  className?: string;
+  style?: React.CSSProperties;
+  /**
+   * 图片源，优先于文字
+   */
+  image?: string;
+  /**
+   * 水印文字内容
+   */
+  content?: string | string[];
+  /**
+   * 追加的水印元素的 z-index
+   */
+  zIndex?: number;
+  /**
+   * 水印的宽度
+   */
+  width?: number;
+  /**
+   * 水印的高度
+   */
+  height?: number;
+  /**
+   * 水印绘制时，旋转的角度
+   */
+  rotate?: number;
+  font?: {
+    color?: CanvasFillStrokeStyles['fillStyle'];
+    fontSize?: number | string;
+    fontWeight?: 'normal' | 'light' | 'weight' | number;
+    fontStyle?: 'none' | 'normal' | 'italic' | 'oblique';
+    fontFamily?: string;
+    textAlign?: CanvasTextAlign;
+  };
+  /**
+   * 水印之间的间距
+   */
+  gap?: [number, number];
+  /**
+   * 水印距离容器左上角的偏移量
+   */
+  offset?: [number, number];
+  inherit?: boolean;
+  children?: React.ReactNode;
+}
