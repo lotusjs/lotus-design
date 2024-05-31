8c33737d391240ce359ab4bb4bfb5b4e06ddddc3
wangxingkang <wang_xingkang@qq.com>
2024-05-31 16:13:48 +0800

    feat: add reset css

--- packages/react/src/aspect-ratio/index.ts
+++ packages/react/src/aspect-ratio/index.ts
@@ -2,5 +2,5 @@ import * as AspectRatioPrimitive from '@lotus-design/react-primitives/aspect-rat
 
 const AspectRatio = AspectRatioPrimitive.Root
 
-export { AspectRatio }
+export default AspectRatio
 export type { AspectRatioProps } from '@lotus-design/react-primitives/aspect-ratio'
