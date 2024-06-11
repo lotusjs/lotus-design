2e8de6bd5974ebdc35ac6f1b2862c7ec34f064f4
wangxingkang <wang_xingkang@qq.com>
2024-06-11 18:19:33 +0800

    chore: 编译输出 components.less

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu(
+  {
+    react: true,
+    stylistic: {
+      semi: true,
+    },
+    ignores: [
+      'scripts',
+    ],
+  },
+);
--- scripts/tasks/buildStyle.ts
+++ scripts/tasks/buildStyle.ts
@@ -1,5 +1,6 @@
 import gulp from 'gulp'
-import { join } from 'node:path';
+import path from 'node:path'
+import fs from 'node:fs'
 // @ts-ignore
 import less from 'gulp-less'
 import postcss from 'gulp-postcss'
@@ -15,7 +16,7 @@ export async function buildStyle(opts: Options = {}) {
   const { cwd = process.cwd(), esDir = 'es', libDir = 'lib' } = opts;
 
   function copyLess() {
-    return  gulp
+    return gulp
       .src(['./src/**/*.less'], {
         base: './src/',
         cwd,
@@ -23,6 +24,32 @@ export async function buildStyle(opts: Options = {}) {
       })
       .pipe(gulp.dest(`./${esDir}`))
       .pipe(gulp.dest(`./${libDir}`))
+  }
+
+  function componentLess(cb: () => void) {
+    const componentsPath = path.join(process.cwd(), 'src');
+    let componentsLessContent = '';
+    const files = fs.readdirSync(componentsPath)
+
+    files.forEach((file) => {
+      if (fs.existsSync(path.join(componentsPath, file, 'style', 'index.less'))) {
+        componentsLessContent += `@import "../${path.posix.join(
+          file,
+          'style',
+          'index.less',
+        )}";\n`;
+      }
+    })
+
+    fs.writeFileSync(
+      path.join(process.cwd(), 'lib', 'style', 'components.less'),
+      componentsLessContent,
+    );
+    fs.writeFileSync(
+      path.join(process.cwd(), 'es', 'style', 'components.less'),
+      componentsLessContent,
+    );
+    cb();
   }
 
   function buildLess() {
@@ -34,20 +61,18 @@ export async function buildStyle(opts: Options = {}) {
       })
       .pipe(
         less({
-          paths: [join(cwd, 'src')],
+          paths: [path.join(cwd, 'src')],
           relativeUrls: true,
         })
       )
       .pipe(
         postcss([
-          autoprefixer({
-            overrideBrowserslist: 'iOS >= 10, Chrome >= 49',
-          }),
+          autoprefixer(),
         ])
       )
       .pipe(gulp.dest(`./${esDir}`))
       .pipe(gulp.dest(`./${libDir}`))
   }
 
-  return gulp.parallel(copyLess, buildLess)(() => {})
+  return gulp.series(gulp.parallel(copyLess, buildLess), componentLess)(() => {})
 }
